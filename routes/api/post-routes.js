df9f6894b1e29925611442c88d14389155924976
Adam <ats92@cox.net>
2021-10-26 14:01:05 -0700

    endpoints

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     console.log('========================');
@@ -13,11 +13,20 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+
             {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
                 model: User,
                 attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
             }
-        ]
+          ]
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -38,6 +47,14 @@ router.get('/:id', (req, res) => {
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
       include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
         {
           model: User,
           attributes: ['username']
