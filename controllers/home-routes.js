79d570fd9b2a928f5589bde31fa3e33522fa7a8f
Adam <ats92@cox.net>
2021-10-28 18:23:51 -0700

    auth

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,4 +32,14 @@ router.get('/', (req, res) => {
         res.render('homepage', {posts});
     });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 module.exports = router;
